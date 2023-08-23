8c9e79bf1916a835100e7b35f84028add665b22e
yong.teng <yong.teng@liangvi.com>
2023-08-23 16:11:40 +0800

    更改名称和统计代码

--- src/js/stat.js
+++ src/js/stat.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?f04c577c4cd5d961fad5cf491559e973";
+  hm.src = "https://hm.baidu.com/hm.js?d9dc323750cc3d97dd18565629fd5119";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
